660a670daa85173fb8eaa38bc1f81f6d527d9b94
subiksha <subikshaportkarani@gmail.com>
2026-07-07 14:07:14 +0530

    input and status page

--- helloflask.py
+++ helloflask.py
@@ -2,16 +2,17 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/inputpage")
-def inputpage():
-    return render_template("inputpage.html")
+#@app.route('/inputpage')
+def input_page():
+    return render_template('inputpage.html')
 
 
-@app.route("/statuspage")
-def statuspage():
-    inputvalue = request.args.get("textinput")
-    return render_template("statuspage.html", inputvalue=inputvalue)
+@app.route("/statuspage", methods=['GET'])
+def status_page():
+    # Extracts the text entered from the "textinput" field in the form
+    status = request.args.get("textinput")
+    return render_template('statuspage.html', status = status)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
