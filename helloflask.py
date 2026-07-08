efe7b52d77720ab37ad999542155a65424e4c5f0
subiksha <subikshaportkarani@gmail.com>
2026-07-08 13:08:58 +0530

    sqllite database

--- helloflask.py
+++ helloflask.py
@@ -1,22 +1,38 @@
 from flask import Flask, render_template, request
+import sqlite3
 
 app = Flask(__name__)
 
-@app.route('/input')
-def home():
-    return render_template('inputpage.html')
+conn = sqlite3.connect("database.db", check_same_thread=False)
+cursor = conn.cursor()
 
-@app.route('/output', methods=['POST'])
-def output():
-    name = request.form['name']
-    
-    students = ["bobo", "Bowbow", "lil"]
-    
-    return render_template(
-        'outputpage.html',
-        name=name,
-        students=students
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS student(
+    name TEXT,
+    age INTEGER
+)
+""")
+conn.commit()
+
+@app.route("/input")
+def input():
+    return render_template("inputpage.html")
+
+@app.route("/save", methods=["POST"])
+def save():
+
+    name = request.form.get("name")
+    age = request.form.get("age")
+
+    cursor.execute(
+        "INSERT INTO student(name, age) VALUES(?, ?)",
+        (name, age)
     )
 
-if __name__ == '__main__':
+    conn.commit()
+
+    return "Data Saved Successfully"
+
+if __name__ == "__main__":
     app.run(debug=True)
+ 
