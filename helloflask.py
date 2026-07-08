14d70150e5e0ca1387c8ae2bbc3af6ea79cc8472
subiksha <subikshaportkarani@gmail.com>
2026-07-08 10:50:19 +0530

    POST From Method

--- helloflask.py
+++ helloflask.py
@@ -1,18 +1,17 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template,redirect, request
 
 app = Flask(__name__)
 
-#@app.route('/inputpage')
-def input_page():
-    return render_template('inputpage.html')
+@app.route("/inputpage")
+def inputpage():
+    return render_template("inputpage.html")
 
 
-@app.route("/statuspage", methods=['GET'])
-def status_page():
-    # Extracts the text entered from the "textinput" field in the form
-    status = request.args.get("textinput")
-    return render_template('statuspage.html', status = status)
+@app.route("/statuspage",methods=["POST"])
+def statuspage():
+    status = request.form.get("textinput")
+    return render_template("statuspage.html", status=status)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
